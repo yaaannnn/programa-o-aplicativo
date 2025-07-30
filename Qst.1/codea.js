53aa2f2b268b2f0eabd919a467578226e3c20978
yaaannnn <xyanx2000@gmail.com>
2025-07-30 19:44:04 -0300

    Qst.1 Completa

--- Qst.1/codea.js
+++ Qst.1/codea.js
@@ -1,5 +1,5 @@
 const prompt = require("prompt-sync")();
 
 let nome = prompt ("Digite seu nome: ");
-let idade = prompt ("Digite sua idade: ")
+let idade = prompt ("Digite sua idade: ");
 console.log("Bem-Vindo",nome);
