5dd9853991e08bee196f4e9f98c9adbb68d03ffc
yaaannnn <xyanx2000@gmail.com>
2025-08-04 11:18:40 -0300

    Terminar

---
+++ Qst.3/code1.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")();
+let pontos = 0;
+
+for (let contador = 1; contador <= 10; contador++){
+    console.log(`Batalha${contador}: Você gnhou 10 pontos de experiência!`);
+    pontos += 10;
+}
+console.log(`Experiência total: ${pontos}`);
---
+++ Qst.3/code2.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+
+for (let i = 2; i <= 9; i++) {
+    console.log(`Tabuada do ${i}:`);
+    
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+    }
+    console.log(''); 
+}
